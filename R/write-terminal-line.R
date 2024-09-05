0ec9354d71d87045bc9847263379135c8dc11cdb
Luke Rogers <luke.allister.rogers@gmail.com>
2024-09-04 17:02:33 -0700

    updated write_terminal_line()

---
+++ R/write-terminal-line.R
@@ -0,0 +1,35 @@
+#' Write Stock Synthesis 3.30 Data Frame Terminal Line
+#'
+#' @param n [integer()]
+#' @param file [character()]
+#' @param value [numeric()]
+#' @param name [character()]
+#' @param append [logical()]
+#'
+#' @return [character()]
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' write_terminal_line(n = 10, file = "temp.ss")
+#' }
+#'
+write_terminal_line <- function (n,
+																 file,
+																 value = -9999,
+																 name = "terminal line",
+																 append = TRUE) {
+
+	# Define ---------------------------------------------------------------------
+
+	x <- paste(value, paste(rep(0, n-1), collapse = " "), "#", name, sep = c(" "))
+
+	# Write ----------------------------------------------------------------------
+
+	readr::write_lines(x = x, file = file, append = append)
+
+	# Return ---------------------------------------------------------------------
+
+	return(invisible(x))
+
+}
