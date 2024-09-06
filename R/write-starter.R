9376a2d779f3d02b655b3bf2c4353eb2df114544
Luke Rogers <luke.allister.rogers@gmail.com>
2024-09-06 13:04:14 -0700

    streamlined write_starter()

--- R/write-starter.R
+++ R/write-starter.R
@@ -5,23 +5,23 @@
 #' @param file_name_control [character()]
 #' @param use_ss3_dot_par [integer()]
 #' @param progress_detail [integer()]
-#' @param option_age_report [integer()]
-#' @param option_custom_report [integer()]
+#' @param age_report_option [integer()]
+#' @param custom_report_option [integer()]
 #' @param parameter_trace [integer()]
-#' @param option_cumulative_report [integer()]
+#' @param cumulative_report_option [integer()]
 #' @param use_full_priors [integer()]
 #' @param use_soft_bounds [integer()]
-#' @param option_data_output [integer()]
+#' @param data_output_option [integer()]
 #' @param phase_maximum [integer()]
 #' @param mcmc_burn [integer()]
 #' @param mcmc_thin [integer()]
-#' @param option_jitter [numeric()]
-#' @param year_min_sd_report [integer()]
-#' @param year_max_sd_report [integer()]
-#' @param years_extra_sd_report [integer()]
+#' @param jitter_option [numeric()]
+#' @param sd_report_year_min [integer()]
+#' @param sd_report_year_max [integer()]
+#' @param sd_report_years_extra [integer()]
 #' @param convergence_value [numeric()]
 #' @param year_retrospective [integer()]
-#' @param option_age_summary [integer()]
+#' @param age_summary_option [integer()]
 #' @param depletion_basis [integer()]
 #' @param depletion_denom [numeric()]
 #' @param spr_basis [integer()]
@@ -45,23 +45,23 @@ write_starter <- function (file = "starter.ss",
 													 file_name_control = "control.ss",
 													 use_ss3_dot_par = 0,
 													 progress_detail = 0,
-													 option_age_report = 1,
-													 option_custom_report = 0,
+													 age_report_option = 1,
+													 custom_report_option = 0,
 													 parameter_trace = 0,
-													 option_cumulative_report = 2,
+													 cumulative_report_option = 2,
 													 use_full_priors = 0,
 													 use_soft_bounds = 1,
-													 option_data_output = 1,
+													 data_output_option = 1,
 													 phase_maximum = 10,
 													 mcmc_burn = 250,
 													 mcmc_thin = 5,
-													 option_jitter = 0,
-													 year_min_sd_report = -1, #
-													 year_max_sd_report = -1, #
-													 years_extra_sd_report = 0,
+													 jitter_option = 0,
+													 sd_report_year_min = -1, #
+													 sd_report_year_max = -1, #
+													 sd_report_years_extra = 0,
 													 convergence_value = 1e-04,
 													 year_retrospective = 0,
-													 option_age_summary = 1,
+													 age_summary_option = 1,
 													 depletion_basis = 1,
 													 depletion_denom = 1,
 													 spr_basis = 0,
@@ -74,46 +74,44 @@ write_starter <- function (file = "starter.ss",
 
 	# Define ---------------------------------------------------------------------
 
-	# Separation string
-	sep <- c(" # ")
-	# Vector
-	x <- list(
-		paste(c("#C starter file written by ssio::write_starter()")),
-		paste(file_name_data, "data file name", sep = sep),
-		paste(file_name_control, "control file name", sep = sep),
-		paste(use_ss3_dot_par, "use ss3 dot par", sep = sep),
-		paste(progress_detail, "progress detail", sep = sep),
-		paste(option_age_report, "option age report", sep = sep),
-		paste(option_custom_report, "option custom report", sep = sep),
-		paste(parameter_trace, "parameter trace", sep = sep),
-		paste(option_cumulative_report, "option cumulative report", sep = sep),
-		paste(use_full_priors, "use full priors", sep = sep),
-		paste(use_soft_bounds, "use soft bounds", sep = sep),
-		paste(option_data_output, "option data output", sep = sep),
-		paste(phase_maximum, "phase maximum", sep = sep),
-		paste(mcmc_burn, "mcmc burn", sep = sep),
-		paste(mcmc_thin, "mcmc thin", sep = sep),
-		paste(option_jitter, "option jitter", sep = sep),
-		paste(year_min_sd_report, "year min sd report", sep = sep),
-		paste(year_max_sd_report, "year max sd report", sep = sep),
-		paste(years_extra_sd_report, "years extra sd report", sep = sep),
-		paste(convergence_value, "convergence value", sep = sep),
-		paste(year_retrospective, "year retrospective", sep = sep),
-		paste(option_age_summary, "option age summary", sep = sep),
-		paste(depletion_basis, "depletion basis", sep = sep),
-		paste(depletion_denom, "depletion denominator", sep = sep),
-		paste(spr_basis, "spr basis", sep = sep),
-		paste(fishing_units, "fishing units", sep = sep),
-		paste(fishing_basis, "fishing basis", sep = sep),
-		paste(mcmc_detail, "mcmc detail", sep = sep),
-		paste(alk_tolerance, "alk tolerance", sep = sep),
-		paste(boot_seed, "boot seed", sep = sep),
-		paste(check_value, "check value", sep = sep)
-	)
+	x <- as.list(environment())
+
+	# Comments -------------------------------------------------------------------
+
+	write_comment_line("written by ssio::write_starter()", file, append = FALSE)
 
 	# Write ----------------------------------------------------------------------
 
-	readr::write_lines(x = x, file = file)
+	write_argument_line(file_name_data, file, append = TRUE)
+	write_argument_line(file_name_control, file, append = TRUE)
+	write_argument_line(use_ss3_dot_par, file, append = TRUE)
+	write_argument_line(progress_detail, file, append = TRUE)
+	write_argument_line(age_report_option, file, append = TRUE)
+	write_argument_line(custom_report_option, file, append = TRUE)
+	write_argument_line(parameter_trace, file, append = TRUE)
+	write_argument_line(cumulative_report_option, file, append = TRUE)
+	write_argument_line(use_full_priors, file, append = TRUE)
+	write_argument_line(use_soft_bounds, file, append = TRUE)
+	write_argument_line(data_output_option, file, append = TRUE)
+	write_argument_line(phase_maximum, file, append = TRUE)
+	write_argument_line(mcmc_burn, file, append = TRUE)
+	write_argument_line(mcmc_thin, file, append = TRUE)
+	write_argument_line(jitter_option, file, append = TRUE)
+	write_argument_line(sd_report_year_min, file, append = TRUE)
+	write_argument_line(sd_report_year_max, file, append = TRUE)
+	write_argument_line(sd_report_years_extra, file, append = TRUE)
+	write_argument_line(convergence_value, file, append = TRUE)
+	write_argument_line(year_retrospective, file, append = TRUE)
+	write_argument_line(age_summary_option, file, append = TRUE)
+	write_argument_line(depletion_basis, file, append = TRUE)
+	write_argument_line(depletion_denom, file, append = TRUE)
+	write_argument_line(spr_basis, file, append = TRUE)
+	write_argument_line(fishing_units, file, append = TRUE)
+	write_argument_line(fishing_basis, file, append = TRUE)
+	write_argument_line(mcmc_detail, file, append = TRUE)
+	write_argument_line(alk_tolerance, file, append = TRUE)
+	write_argument_line(boot_seed, file, append = TRUE)
+	write_argument_line(check_value, file, append = TRUE)
 
 	# Return ---------------------------------------------------------------------
 
