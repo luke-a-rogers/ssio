aa797c8a4c3287eed592dd56f399712e525e6eda
Luke Rogers <luke.allister.rogers@gmail.com>
2024-10-16 18:20:02 -0700

    began catch functions to simplify discard scaling

---
+++ R/catch.R
@@ -0,0 +1,24 @@
+
+catch_save <- function (x, file = "data/ss3/catch-ssio.rds") {
+
+}
+
+catch_read <- function (file = "data/ss3/catch-ssio.rds") {
+
+}
+
+catch_scale <- function (x, gear = NULL, type = NULL) {
+
+}
+
+catch_combine <- function (x, gear = NULL) {
+
+}
+
+catch_renumber <- function (x) {
+
+}
+
+catch_format <- function (x) {
+
+}
